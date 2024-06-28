a03f7ed397ee942ede22cec66848b9a319333471
Ignacio Revainera <ignacio.revainera@hotmail.com>
2024-06-28 12:02:53 -0300

    Add initial configuration, files and components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://localhost:4321",
+  integrations: [tailwind(), sitemap({
+    i18n: {
+      defaultLocale: "es",
+      locales: ["es", "en"]
+    }
+  }), icon()]
+});
